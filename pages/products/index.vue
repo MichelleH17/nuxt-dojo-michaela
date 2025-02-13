e7fa626bdb56efd27ac8ec97bbc74df1d145f1e0
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-13 20:42:20 +0100

    feat: update products page to display product list in a grid layout

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <h2>Products</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim iure provident accusamus vitae ab facere atque nostrum fugit quae quidem placeat libero, consequuntur tempore quasi veniam laboriosam ex sint voluptate.</p>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="product in products">
+        <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script lang="ts" setup>
   definePageMeta({
-      layout: 'products'
+    layout: 'products'
   })
+
+  const { data: products } = await useFetch<IProduct[]>('https://fakestoreapi.com/products')
 </script>
 
 <style scoped>
