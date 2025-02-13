c2577f720cf40e1edfc63c3d503a9c4285650d09
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-13 12:15:01 +0100

    feat: add navigation to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,35 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
     <h2>Home</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In facere sunt corrupti suscipit numquam cupiditate labore iusto, deserunt consectetur maxime.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam obcaecati id doloribus eos alias eveniet vitae perferendis, sunt voluptas tempore.</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In facere sunt
+      corrupti suscipit numquam cupiditate labore iusto, deserunt consectetur
+      maxime.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam obcaecati
+      id doloribus eos alias eveniet vitae perferendis, sunt voluptas tempore.
+    </p>
   </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <style scoped>
-  h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
-  }
-  p {
-    margin: 20px 0;
-  }
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
 </style>
