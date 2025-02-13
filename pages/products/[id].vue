92d664871983afd1867b95e1b6e37f9a601103b3
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-13 12:10:19 +0100

    feat: add product details page with dynamic routing

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos dolores architecto veritatis ratione cumque necessitatibus deserunt dolor voluptatem cupiditate autem!</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
