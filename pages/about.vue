bd528f6ae775132fe3acf6ef5f911b1fa69f9eaa
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-13 11:46:01 +0100

    feat: add about and home pages with basic content and styling

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In facere sunt corrupti suscipit numquam cupiditate labore iusto, deserunt consectetur maxime.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam obcaecati id doloribus eos alias eveniet vitae perferendis, sunt voluptas tempore.</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
