22e7a6ede7fc674e1ad9cc67550d1c11a5c488cd
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-13 13:45:45 +0100

    feat: use products layout instead of default layout

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <p>Products</p>
+    <h2>Products</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim iure provident accusamus vitae ab facere atque nostrum fugit quae quidem placeat libero, consequuntur tempore quasi veniam laboriosam ex sint voluptate.</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+  definePageMeta({
+      layout: 'products'
+  })
 </script>
 
 <style scoped>
