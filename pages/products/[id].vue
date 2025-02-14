30763ea71b2bac6fceff6d3fe7778deae32f75e3
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-14 08:24:48 +0100

    feat: add nuxt components head, title and meta to product details page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <Head>
+      <Title>Nuxt Dojo | {{ product?.title }}</Title>
+      <Meta name="description" :content="product?.description" />
+    </Head>
+    
     <div v-if="product">
       <ProductDetails :product="product" />  
     </div>
