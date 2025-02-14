18f341601db55f59326a023ba07039176f474c00
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-14 08:16:58 +0100

    feat: add app default values of title and meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Nuxt Dojo',
+      meta: [
+        { name: 'description', content: 'Learning Nuxt 3' },
+      ]    
+    }
+  }
 })
